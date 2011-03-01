85509beefa965eb3314da8ebbed3a736eefbfac4
cweybrec <cweybrec@cs.kent.edu>
2011-03-01 16:14:03 -0500

    Adding get_module_fields function class.

---
+++ api/GetModuleFields.class.php
@@ -0,0 +1,33 @@
+<?php
+    require_once 'api/ApiFunction.class.php';
+    
+    class Api_GetModuleFields extends Api_ApiFunction
+    {
+        private $_sessionId;
+        
+        private $_moduleName;
+        
+        public function __construct()
+        {
+            parent::__construct();
+        }
+        
+        public function setSessionId($session_id)
+        {
+            $this->_sessionId = $session_id;
+        }
+        
+        public function setModuleName($module_name)
+        {
+            $this->_moduleName = $module_name;
+        }
+        
+        protected function buildParameters()
+        {
+            return array(
+                'session' => $this->_sessionId,
+                'module_name' => $this->_moduleName,
+            );
+        }
+    }
+?>
