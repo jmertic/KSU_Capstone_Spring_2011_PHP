54451a7a5bb212b25f90ff28b145d85b9d5d97f5
cweybrec <cweybrec@cs.kent.edu>
2011-03-01 16:13:51 -0500

    Adding set_entry function class.

---
+++ api/SetEntry.class.php
@@ -0,0 +1,41 @@
+<?php
+    require_once 'api/ApiFunction.class.php';
+    
+    class Api_SetEntry extends Api_ApiFunction
+    {
+        private $_sessionId;
+        
+        private $_moduleName;
+        
+        private $_nameValueList;
+        
+        public function __construct()
+        {
+            parent::__construct();
+        }
+        
+        public function setSessionId($session_id)
+        {
+            $this->_sessionId = $session_id;
+        }
+        
+        public function setModuleName($module_name)
+        {
+            $this->_moduleName = $module_name;
+        }
+        
+        public function setNameValueList($array)
+        {
+            $this->_nameValueList = $array;
+        }
+        
+        protected function buildParameters()
+        {
+            return array(
+                'session' => $this->_sessionId,
+                'module' => $this->_moduleName,
+                'name_value_list' => $this->_nameValueList,
+            );
+        }
+    }
+?>
