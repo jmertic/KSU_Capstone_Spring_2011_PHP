69aa269d9d4fd6d2afaa6e832d98b83833124498
Alrusayni <aalrusay@kent.edu>
2011-03-08 16:58:10 -0500

    Adding Seamless login function class

---
+++ api/SeamlessLogin.class.php
@@ -0,0 +1,32 @@
+<?php
+	/**
+     * Seamless Login class for the Sugar CRM API
+     * 
+     * @author Amin Alrusayni <aalrusay@kent.edu>
+     */
+
+	require_once 'ApiFunction.class.php';
+	
+	class Api_SeamlessLogin extends Api_ApiFunction
+	{
+		private $_sessionId;
+	
+	
+		public function __construct()
+		{
+			parent::__construct();
+		}
+		
+		public function setSessionId($session_id)
+		{
+			$this->_sessionId = $session_id;
+		}
+		
+		protected  function buildParameters()
+		{
+			return array(
+				'session' => $this->_sessionId,
+			);
+		}
+}
+?>
