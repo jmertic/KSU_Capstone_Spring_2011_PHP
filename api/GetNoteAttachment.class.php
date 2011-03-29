e9c830346ebb8b7d7fd6e6dea95477158d507eb4
yguzaiz <yguzaiz@kent.edu>
2011-03-29 16:30:59 -0400

    Addin GetNoteAttachment

---
+++ api/GetNoteAttachment.class.php
@@ -0,0 +1,45 @@
+//author Yousef Guzaiz
+
+<?php
+    require_once 'ApiFunction.class.php';
+
+class Api_GetNoteAttachment extends Api_ApiFunction
+{
+//session, id)
+        private $_sessionId;
+        private $_id;
+
+ public function __construct()
+        {
+            parent::__construct();
+        }
+
+  public function setSessionId($session_id)
+        {
+            $this->_sessionId = $session_id;
+        }
+
+ public function setId($ID)
+        {
+            $this->_id = $ID;
+        }
+
+protected function buildParameters()
+        {
+		if (empty($this->_sessionId)) {
+                throw new Exception('Session ID not set');
+            }
+
+            return array(
+                'session' => $this->_sessionId,
+                'id' => $this->_id,
+            );
+        }
+
+
+
+}//end
+
+
+?>
+
