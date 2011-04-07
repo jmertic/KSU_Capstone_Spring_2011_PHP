f96abdfffeebcc43efe69b773a0a5700a1da767a
Alrusayni <aalrusay@kent.edu>
2011-04-07 16:33:57 -0400

    Fix

--- api/GetUserId.class.php
+++ api/GetUserId.class.php
@@ -10,9 +10,7 @@
 		class Api_GetUserId extends Api_ApiFunction
 		{
 				private $_sessionId;
-				
-				private $_userId;
-				
+						
 				public  function __construct()
 				{
 					parent:: __construct();
@@ -21,21 +19,12 @@
 				public function setSessionId($session_id)
 				{
 					$this->_sessionId = $session_id;
-				}
-				
-				public function setUserId($user_id)
-				{
-					$this->_userId = $user_id;
-				}
+				}	
 				
 				protected  function buildParameters()
 				{
 					if (empty($this->_sessionId)) {
             			throw new Exception('Session ID not set');
-            		}
-            		
-				if (empty($this->_userId)) {
-            			throw new Exception('User ID not set');
             		}
             		
 		        return $parameters;
