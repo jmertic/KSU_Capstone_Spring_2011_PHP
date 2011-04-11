ed28a11a093fad689e36dbfd04d7e04e68e27f4b
Nathan Osysko <nosysko@kent.edu>
2011-04-11 19:36:41 -0400

    BUG FIX

--- api/GetEntries.class.php
+++ api/GetEntries.class.php
@@ -7,8 +7,8 @@
      * @author Nathan Osysko
      * @package api
      */
-     class Api_GetEntries extends Api_ApiFunction
-     {
+    class Api_GetEntries extends Api_ApiFunction
+    {
         /**
          * Session ID
          *
