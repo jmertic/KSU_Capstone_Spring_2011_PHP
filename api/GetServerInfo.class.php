848263b91a1140b799f9db38c80504f0cab807ec
yguzaiz <yguzaiz@kent.edu>
2011-04-12 19:23:05 -0700

    Edited api/GetServerInfo.class.php via GitHub

--- api/GetServerInfo.class.php
+++ api/GetServerInfo.class.php
@@ -1,18 +1,30 @@
 <?php
-   //author Yousef Guzaiz  <yguzaiz@cs.kent.edu>
  require_once 'ApiFunction.class.php';
 
+/**
+*Returns server information such as version, flavor, and gmt_time.
+*
+*@author Yousef Guzaiz
+*@package api
+*/
 
 class Api_GetServerInfo  extends Api_ApiFunction
 {
+
+/**
+* Constructor
+*/
 
 public function __construct()
         {
             parent::__construct();
         }
 
-
-
+/**
+* Builds the parameter array
+*
+* @return $parameters
+*/
 
 protected function buildParameters()
         {
